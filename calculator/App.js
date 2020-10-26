0f90bce7cb40249f9bd5a4358eeb52b8f4b11ce9
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-10-26 17:16:31 +0200

    added calculator

--- calculator/App.js
+++ calculator/App.js
@@ -1,21 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState,Component} from 'react';
+import {StyleSheet,View, Button, TextInput, Text, Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={Number1:0,Number2:0};
+  }
+
+  Sum = () => {
+    var Num1 = parseInt(this.state.Number1);
+    var Num2 = parseInt(this.state.Number2);
+
+    var result = Num1 + Num2;
+
+    Alert.alert('Your result is: ' + result);
+
+  }
+
+  Subtract = () => {
+    var Num1 = parseInt(this.state.Number1);
+    var Num2 = parseInt(this.state.Number2);
+
+    var result = Num1 - Num2;
+
+    Alert.alert('Your result is: ' + result);
+
+  }
+ 
+  
+
+  render()
+  {
+    return(
+      <View style={styles.container}>
+          
+          <TextInput style={{borderWidth:1, margin:10}} placeholder="Number1" onChangeText={Number1 => this.setState({Number1})} keyboardType={'numeric'}/>
+
+          <TextInput style={{borderWidth:1, margin:10}} placeholder="Number2" onChangeText={Number2 => this.setState({Number2})} keyboardType={'numeric'}/>
+
+          <Button color="green"title="Sum" onPress={this.Sum}/>
+          <Button color="red" title="Subtract" onPress={this.Subtract}/>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    
     justifyContent: 'center',
   },
 });
+
