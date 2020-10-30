b80e13aa7e01358f796b1c589adea14505a5a380
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-10-30 15:28:34 +0200

    shopping list

--- shopping_list/App.js
+++ shopping_list/App.js
@@ -1,13 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button,TextInput,FlatList } from 'react-native';
 
+
+
+
+  
 export default function App() {
+  const [shopitem, setShopitem] = useState('');
+  const [data, setData] = useState([]);
+
+   const Add = () => {  
+    setData([...data, {key:shopitem}]);
+    setShopitem('');
+  }
+  const Clear = () => {
+    setData([]);
+   
+  }
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>Add items to your shopping list</Text>
+    <TextInput style={{borderWidth:1, margin:10,width:100}}
+    placeholder=""
+    onChangeText={text => setShopitem(text)}
+    value={shopitem}
+    ></TextInput>
+
+
+    
+    <View style={styles.button}>
+    <Button color="green" title="ADD" onPress={Add}></Button>
     </View>
+    <View style={styles.button}>
+    <Button color="red" title="CLEAR" onPress={Clear}></Button>
+    </View>
+    <Text>Shopping List:</Text>
+    <FlatList
+    keyExtractor={() => Math.random().toString(36).substr(2, 9)}
+      data={data}
+      renderItem={({item}) =>
+      <Text>{item.key}</Text>}
+      
+    />
+    
+</View>
   );
 }
 
@@ -17,5 +56,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:100
   },
+  button:{
+    width:100,
+    margin:2
+  }
 });
