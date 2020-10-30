508461221b01793f7e0515cb77430f0cb8c80935
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-10-30 14:59:17 +0200

    calculator with history

--- calculator_with_history/App.js
+++ calculator_with_history/App.js
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button,TextInput,FlatList } from 'react-native';
 
+
+
+
+  
 export default function App() {
+  
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+  const [data, setData] = useState([]);
+  
+  
+
+  const Sum = () => {
+    setResult(parseInt(number1)+parseInt(number2));
+    var r =(parseInt(number1)+parseInt(number2));
+    setData([...data, {history: number1 + "+" + number2+ "=" + r}]);
+    
+    
+    
+  }
+  const Subtract = () => {
+    setResult(parseInt(number1)-parseInt(number2));
+    var r =(parseInt(number1)-parseInt(number2));
+    setData([...data, {history: number1 + "-" + number2+ "=" + r}]);
+    
+  }
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+          <Text>Result:{result}</Text>
+          
+    <TextInput style={{borderWidth:1, margin:10,width:100}}
+    placeholder="Number1"
+    onChangeText={text => setNumber1(text)}
+    value ={number1}
+    keyboardType={'numeric'}></TextInput>
+
+<TextInput style={{borderWidth:1, margin:10,width:100}}
+    placeholder="Number2"
+    onChangeText={text => setNumber2(text)}
+    value ={number2}
+    keyboardType={'numeric'}></TextInput>
+    
+    <View style={styles.button}>
+    <Button color="green" title="SUM" onPress={Sum}></Button>
     </View>
+    <View style={styles.button}>
+    <Button color="red" title="SUBTRACT" onPress={Subtract}></Button>
+    </View>
+    <Text>History:</Text>
+    <FlatList
+    keyExtractor={() => Math.random().toString(36).substr(2, 9)}
+      data={data}
+      renderItem={({item}) =>
+      <Text>{item.history}</Text>}
+      
+    />
+    
+</View>
   );
 }
 
@@ -17,5 +72,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:100
   },
+  button:{
+    width:100,
+    margin:2
+  }
 });
