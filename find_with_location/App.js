36a7283674ed242e959cd83a4509300dd01a9f35
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-11-12 16:13:39 +0200

    find with location

--- find_with_location/App.js
+++ find_with_location/App.js
@@ -1,26 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 import {  Alert, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
 
 export default function App() {
   const [address, setAddress] = useState('');
-  const [lat, setLatitude] = useState(60.200692);
-  const [lng, setLongitude] = useState(24.934302);
+  const [lat, setLatitude] = useState(0.0);
+  const [lng, setLongitude] = useState(0.0);
+
+  useEffect(() => {
+    getLocation();
+  },[]);
+  
+  const getLocation = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted'){
+      Alert.alert('No permission to access location');
+    } else {
+      let location = await Location.getCurrentPositionAsync({});
+      setLatitude(parseFloat(location.coords.latitude));
+      setLongitude(parseFloat(location.coords.longitude));
+    }
+  };
+
+
 
 
   const findAddress = async () => {
-    const url = 'http://www.mapquestapi.com/geocoding/v1/address?key=6blSbuRVtAv5BCYiPyO1XcRDAL3AfDA5&maxResults=1&location='+address;
-    
+    const url = 'http://www.mapquestapi.com/geocoding/v1/address?key=6blSbuRVtAv5BCYiPyO1XcRDAL3AfDA5&maxResults=1&location=' + address;
+
     try {
       const response = await fetch(url);
       const mq = await response.json();
-      
+
       setLatitude(parseFloat(mq.results[0].locations[0].latLng.lat));
       setLongitude(parseFloat(mq.results[0].locations[0].latLng.lng));
-     
-    } catch(e){
-      Alert.alert(url)
+
+    } catch (e) {
+      Alert.alert(url);
     }
   }
   return (
@@ -30,8 +49,8 @@ export default function App() {
         initialRegion={{
           latitude:lat,
           longitude:lng,
-          latitudeDelta: 100.0,
-          longitudeDelta: 100.0,
+          latitudeDelta: 50.3322,
+          longitudeDelta: 50.3221,
         }}
         region={{
           latitude:lat,
